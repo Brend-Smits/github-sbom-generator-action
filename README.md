892578ecc1bf1c82268b05affd1cb1f524df4130
Brend Smits <brend.smits@philips.com>
2023-04-12 23:05:54 +0200

    feat: add script, action and test workflow
    Signed-off-by: Brend Smits <brend.smits@philips.com>

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# retrieve-github-sboms
-Retrieve all requested SBOMs from the GitHub repositories.
+# Retrieve GitHub SBOMs
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+Retrieve GitHub SBOMs is a GitHub Actions composite action that retrieves Software Bill of Materials (SBOMs) for one or multiple repositories from GitHub's Dependency Graph API and saves them to a specified directory.
+
+## Usage
+
+To use this composite action in your workflow, you can include the following step in your workflow YAML file:
+
+```yaml
+- name: Retrieve GitHub SBOMs
+  uses: brend-smits/retrieve-github-sboms@<tag-or-branch>
+  with:
+    repoListPath: <path-to-repo-list-file>
+    saveDirectoryPath: <path-to-save-directory>
+    token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+Replace `<tag-or-branch>` with the specific tag or branch of the composite action that you want to use, `<path-to-repo-list-file>` with the path to a file containing a list of repository names, and `<path-to-save-directory>` with the path to the directory where you want to save the retrieved SBOMs. Also, make sure to provide the `GITHUB_TOKEN` secret in your workflow to authenticate with the GitHub API.
+
+The composite action requires the following input:
+
+- `repoListPath`: The path to a file containing a list of repository names to retrieve SBOMs for.
+- `saveDirectoryPath`: The path to the directory where the retrieved SBOMs will be saved.
+- `token`: The GitHub API token for authentication.
+
+The composite action produces no outputs.
+
+## Example
+
+Here's an example of how you can use the composite action in your workflow:
+
+```YAML
+name: Retrieve SBOMs
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  retrieve-sboms:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Checkout code
+      uses: actions/checkout@v3
+
+    - name: Retrieve GitHub SBOMs
+      uses: brend-smits/retrieve-github-sboms@main
+      with:
+        repoListPath: gh-repos.txt
+        saveDirectoryPath: sboms
+        token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+This workflow retrieves SBOMs for repositories listed in the `gh-repos.txt` file and saves them to a `sboms` directory.
+
+## State
+
+> :warning: Experimental!
+
+This action is new and still undergoing testing. Feel free to give it a try and give suggestions or feedback by creating a new GitHub Issue.
---
+++ get-sboms.sh
@@ -0,0 +1,95 @@
+#! /bin/bash
+
+while getopts f:r:t:d flag; do
+    case "${flag}" in
+    r)
+        repoListPath=${OPTARG}
+        echo "=== Repository List Path set to: $repoListPath ==="
+        ;;
+    t)
+        token=${OPTARG}
+        ;;
+    d)
+        echo "=== Debug Mode Enabled ==="
+        set -x
+        ;;
+    f)
+        saveDirectoryPath=${OPTARG}
+        echo "=== Save Directory Path set to: $saveDirectoryPath ==="
+        ;;
+    *)
+        echo "usage: $0 [-r] [-t] [-d] [-f]" >&2
+        exit 1
+        ;;
+    esac
+done
+
+if [ -z "$repoListPath" ]; then
+    echo "=== You did not specify a Repository List Path. Use -r parameter to specify one. ==="
+    exit 1
+fi
+
+if [ -z "$saveDirectoryPath" ]; then
+    echo "=== You did not specify a Save Directory Path. Use -f parameter to specify one. ==="
+    exit 1
+fi
+
+if [ -z "$token" ]; then
+    echo "=== You did not specify a GitHub Token. Use -t parameter to specify one. ==="
+    exit 1
+fi
+
+# Function to call GitHub API and retrieve SBOM for a repository
+get_sbom() {
+  local repo_name=$1
+  local save_directory=$2
+
+  # Call GitHub API to retrieve SBOM
+  local response=$(curl -s -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${token}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/${repo_name}/dependency-graph/sbom")
+
+  # Check for rate limit exceeded error
+  if [[ $response == *"API rate limit exceeded"* ]]; then
+    echo "Error: API rate limit exceeded"
+    exit 1
+  fi
+
+  # Check for other errors
+  if [[ $response == *"404 Not Found"* ]]; then
+    echo "Error: Repository not found: ${repo_name}"
+    return
+  elif [[ $response == *"403 Forbidden"* ]]; then
+    echo "Error: Forbidden: ${repo_name}"
+    return
+  fi
+
+  # Create directory for SBOM if it doesn't exist
+  if [[ ! -d "${save_directory}/${repo_name}" ]]; then
+    mkdir -p "${save_directory}/${repo_name}"
+  fi
+
+  # Save SBOM to file
+  echo "$response" >"${save_directory}/${repo_name}/sbom.json"
+
+  echo "SBOM saved for repository: ${repo_name}"
+}
+
+# Function to process each repository in the list
+process_repo_list() {
+  local repo_list_path=$1
+  local save_directory=$2
+
+  # Loop over each line in the repo list file
+  while IFS= read -r repo_name; do
+    echo "Processing repository: ${repo_name}"
+    get_sbom "${repo_name}" "${save_directory}"
+  done <"${repo_list_path}"
+}
+
+# Call the function with provided variables
+process_repo_list "${repoListPath}" "${saveDirectoryPath}"
+
+
