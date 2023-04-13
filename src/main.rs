6a267790d9f6c34d12529aa9ba17eff4681e567a
Brend Smits <brend.smits@philips.com>
2023-04-13 22:42:04 +0200

    add cli arguments with parser
    Signed-off-by: Brend Smits <brend.smits@philips.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+/// Retrieve GitHub SBOMs is a GitHub Actions composite action that retrieves Software Bill of Materials (SBOMs) for one or multiple repositories from GitHub's Dependency Graph API and saves them to a specified directory.
+#[derive(Parser)]
+struct Cli {
+    #[clap(short, long)]
+    // The path to a file containing a list of repository names to retrieve SBOMs for.
+    repo_list_path: std::path::PathBuf,
+    #[clap(short, long)]
+    // The path to the directory where the retrieved SBOMs will be saved.
+    save_directory_path: std::path::PathBuf,
+    #[clap(short, long)]
+    // The GitHub API token for authentication.
+    token: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("{}", format!("Hello, world! `{}`, `{}`", &args.repo_list_path.display().to_string(), &args.save_directory_path.display().to_string()));
 }
