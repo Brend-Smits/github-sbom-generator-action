06e7d7f749f081e07aa462285c1dfc0deb6d1077
Brend Smits <brend.smits@philips.com>
2023-04-13 23:58:38 +0200

    query gh api using reqwester crate
    Signed-off-by: Brend Smits <brend.smits@philips.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,55 @@
 use clap::Parser;
+use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, AUTHORIZATION, USER_AGENT};
 
 /// Retrieve GitHub SBOMs is a GitHub Actions composite action that retrieves Software Bill of Materials (SBOMs) for one or multiple repositories from GitHub's Dependency Graph API and saves them to a specified directory.
 #[derive(Parser)]
 struct Cli {
     #[clap(short, long)]
-    // The path to a file containing a list of repository names to retrieve SBOMs for.
+    /// The path to a file containing a list of repository names to retrieve SBOMs for.
     repo_list_path: std::path::PathBuf,
     #[clap(short, long)]
-    // The path to the directory where the retrieved SBOMs will be saved.
+    /// The path to the directory where the retrieved SBOMs will be saved.
     save_directory_path: std::path::PathBuf,
     #[clap(short, long)]
-    // The GitHub API token for authentication.
-    token: String
+    /// The GitHub API token for authentication.
+    token: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    println!("{}", format!("Hello, world! `{}`, `{}`", &args.repo_list_path.display().to_string(), &args.save_directory_path.display().to_string()));
+    let repo_name = "brend-smits/retrieve-github-sbom-action";
+    let api_url = format!(
+        "https://api.github.com/repos/{}/dependency-graph/sbom",
+        &repo_name
+    );
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        ACCEPT,
+        HeaderValue::from_static("application/vnd.github+json"),
+    );
+    headers.insert(
+        "X-GitHub-Api-Version",
+        HeaderValue::from_static("2022-11-28"),
+    );
+    headers.insert(
+        AUTHORIZATION,
+        HeaderValue::from_str(&format!("Bearer {}", &args.token))?,
+    );
+    headers.insert(
+        USER_AGENT,
+        HeaderValue::from_static("sbom-downloader-action"),
+    );
+    let client = reqwest::Client::new();
+    let resp = client
+        .get(&api_url)
+        .headers(headers)
+        .send()
+        .await
+        .expect("failed to get response")
+        .text()
+        .await
+        .expect("failed to get payload");
+    println!("{:#?}", resp);
+    Ok(())
 }
