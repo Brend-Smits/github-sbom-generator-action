bacd2759464df02e3814c662e5dcb2618b6ba034
Brend Smits <brend.smits@philips.com>
2023-04-17 13:56:46 +0200

    token is now optional and add various error handling
    Signed-off-by: Brend Smits <brend.smits@philips.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::{BufRead, BufReader};
 
 use clap::Parser;
 use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, AUTHORIZATION, USER_AGENT};
+use serde_json::from_str;
+use serde_json::Value;
 
 /// Retrieve GitHub SBOMs is a GitHub Actions composite action that retrieves Software Bill of Materials (SBOMs) for one or multiple repositories from GitHub's Dependency Graph API and saves them to a specified directory.
 #[derive(Parser)]
@@ -15,7 +17,7 @@ struct Cli {
     save_directory_path: std::path::PathBuf,
     #[clap(short, long)]
     /// The GitHub API token for authentication.
-    token: String,
+    token: Option<String>,
 }
 
 #[derive(Debug)]
@@ -23,14 +25,23 @@ pub struct CustomError(String);
 
 fn main() -> Result<(), CustomError> {
     let args = Cli::parse();
-    read_file_line_by_line(&args.repo_list_path.display().to_string(), &args.token)?;
+    let client = reqwest::Client::new();
+    read_file_line_by_line(args, &client, &fetch_sbom)?;
     Ok(())
 }
 
 // Function to read file line by line and process each line
-fn read_file_line_by_line(repo_list_path: &str, token: &str) -> Result<(), CustomError> {
-    let file = File::open(&repo_list_path)
-        .map_err(|err| CustomError(format!("Error reading `{}`: {}", repo_list_path, err)))?;
+fn read_file_line_by_line<F>(
+    args: Cli,
+    client: &reqwest::Client,
+    process_line: &F,
+) -> Result<(), CustomError>
+where
+    F: Fn(&str, &str, &reqwest::Client) -> Result<(), CustomError>,
+{
+    let file_path = &args.repo_list_path.display().to_string();
+    let file = File::open(&file_path)
+        .map_err(|err| CustomError(format!("Error reading `{}`: {}", file_path, err)))?;
     let reader = BufReader::new(file);
     for line in reader.lines() {
         let content = match line {
@@ -38,17 +49,25 @@ fn read_file_line_by_line(repo_list_path: &str, token: &str) -> Result<(), Custo
             Err(error) => {
                 return Err(CustomError(format!(
                     "Error reading `{}`: {}",
-                    repo_list_path, error
+                    file_path, error
                 )));
             }
         };
-        fetch_sbom(&token, &content).unwrap();
+        process_line(
+            &args.token.as_ref().unwrap_or(&"".to_owned()),
+            &content,
+            client,
+        )?;
     }
     Ok(())
 }
 
 #[tokio::main]
-async fn fetch_sbom(token: &str, repo_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn fetch_sbom(
+    token: &str,
+    repo_name: &str,
+    client: &reqwest::Client,
+) -> Result<(), CustomError> {
     let api_url = format!(
         "https://api.github.com/repos/{}/dependency-graph/sbom",
         &repo_name
@@ -62,24 +81,49 @@ async fn fetch_sbom(token: &str, repo_name: &str) -> Result<(), Box<dyn std::err
         "X-GitHub-Api-Version",
         HeaderValue::from_static("2022-11-28"),
     );
-    headers.insert(
-        AUTHORIZATION,
-        HeaderValue::from_str(&format!("Bearer {}", token))?,
-    );
+    if !token.is_empty() {
+        headers.insert(
+            AUTHORIZATION,
+            HeaderValue::from_str(&format!("Bearer {}", token)).expect("Expects bearer token"),
+        );
+    } else {
+        println!("Token is not set! I can only access some public repositories. Consider using a token with --token option");
+    }
     headers.insert(
         USER_AGENT,
         HeaderValue::from_static("sbom-downloader-action"),
     );
-    let client = reqwest::Client::new();
+    // let client = reqwest::Client::new();
     let resp = client
         .get(&api_url)
         .headers(headers)
         .send()
         .await
-        .expect("failed to get response")
-        .text()
-        .await
-        .expect("failed to get payload");
-    println!("{:#?}", resp);
+        .expect("failed to get response");
+    let response_text = resp.text().await.expect("failed to get payload");
+    if response_text.contains("API rate limit exceeded") {
+        println!("Error: API rate limit exceeded");
+        std::process::exit(1);
+    }
+    if response_text.contains("Not Found") {
+        println!("Error: Repository '{}' not found", repo_name);
+        return Ok(());
+    }
+    if response_text.contains("Bad credentials") {
+        println!("Error: Invalid Token, check token permissions and expiry date!");
+        std::process::exit(1);
+    }
+
+    // Parse the response body as JSON into a SPDX struct
+    let spdx: Value = match from_str(&response_text) {
+        Ok(spdx) => spdx,
+        Err(err) => {
+            return Err(CustomError(format!(
+                "Failed to parse JSON response: {}",
+                err
+            )));
+        }
+    };
+    println!("{:#?}", spdx.to_string());
     Ok(())
 }
